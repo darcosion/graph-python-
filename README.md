9fad54d4bdbe281c71f18b6be09726bb3c37ad01
toopak <toopak@example.org>
2016-05-14 18:25:05 +0200

    ajout de l'ensemble du projet

---
+++ README.md
@@ -0,0 +1,1 @@
+Un simple script et son résultat qui a pour but de montrer à quoi ressemblent par défaut les graph constitués pour chaque cellule d'un arc entrant et d'un arc sortant vers chaque autre cellule.
---
+++ loopgraph.py
@@ -0,0 +1,14 @@
+from graph_tool.all import *
+
+for t in range(1, 51):
+    g = Graph()
+    for i in range(0, t):
+        g.add_vertex()
+
+    for i in range(g.num_vertices()):
+        for e in range(g.num_vertices()):
+            if(i != e):
+                g.add_edge(g.vertex(i), g.vertex(e))
+    graph_draw(g, vertex_text=g.vertex_index, vertex_font_size=18,
+               output_size=(1000, 1000), output="result"+str(t)+".png")
+
