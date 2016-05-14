524a14e1284450c5f3102c5033ed12bca9c2f588
toopak <toopak@example.org>
2016-05-14 19:08:34 +0200

    correction de taille des images

--- loopgraph.py
+++ loopgraph.py
@@ -10,5 +10,5 @@ for t in range(1, 51):
             if(i != e):
                 g.add_edge(g.vertex(i), g.vertex(e))
     graph_draw(g, vertex_text=g.vertex_index, vertex_font_size=18,
-               output_size=(1000, 1000), output="result/result"+str(t)+".png")
+               output_size=(800, 800), output="result/result"+str(t)+".png")
 
